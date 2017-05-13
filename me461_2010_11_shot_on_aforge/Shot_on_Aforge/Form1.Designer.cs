db768dc8ccfc1dcfbabdfafbc0dd8d6cfd4ab8d3
javidkhalilov <javid.khalilov@gmail.com>
2017-05-13 12:01:22 +0300

    Add files via upload

---
+++ me461_2010_11_shot_on_aforge/Shot_on_Aforge/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Shot_on_Aforge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.original = new System.Windows.Forms.PictureBox();
+            this.process = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.original)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.process)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // original
+            // 
+            this.original.Location = new System.Drawing.Point(18, 35);
+            this.original.Name = "original";
+            this.original.Size = new System.Drawing.Size(249, 266);
+            this.original.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.original.TabIndex = 0;
+            this.original.TabStop = false;
+            // 
+            // process
+            // 
+            this.process.Location = new System.Drawing.Point(310, 35);
+            this.process.Name = "process";
+            this.process.Size = new System.Drawing.Size(241, 266);
+            this.process.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.process.TabIndex = 1;
+            this.process.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 334);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(129, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "gray";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(231, 327);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 41);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "edge dedection";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(340, 344);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "binary";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(421, 346);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "100";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(38, 382);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "BlobFilter";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(129, 384);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "200";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(129, 410);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "100";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(129, 436);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "200";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(129, 462);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "100";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(340, 384);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 28);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "video";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(340, 436);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 532);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.process);
+            this.Controls.Add(this.original);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.original)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.process)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox original;
+        private System.Windows.Forms.PictureBox process;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ me461_2010_11_shot_on_aforge/Shot_on_Aforge/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AForge;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace Shot_on_Aforge
+{
+    public partial class Form1 : Form
+    {
+
+        string Filename;
+        Bitmap OriginalBmp,grayBmp,EdgeBmp,BinBmp,BlobBmp;
+        int Threshold; //threshold value of binary filter
+        Blob[] BlobArray = new Blob[10];
+        Boolean GrayIsOn,EdgeBmpIsOn;
+
+
+        static FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+        static VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+
+        Bitmap Video,VideoClone;
+
+        //filters
+        GrayscaleBT709 grayFilter = new GrayscaleBT709(); //Grayscale filter
+        SobelEdgeDetector Sobeledgededector = new SobelEdgeDetector();//sobel edge dedector
+        Threshold BinFilter = new Threshold(); //binary filter
+        BlobsFiltering BlobFilter = new BlobsFiltering();//blob filter
+        BlobCounter BlobCountFilter = new BlobCounter(); //blob counter
+        HoughCircleTransformation circleTransform = new HoughCircleTransformation(100);
+
+        public Form1()
+        {
+            
+            
+            InitializeComponent();
+        }
+
+        void videoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Video = (Bitmap)eventArgs.Frame.Clone();
+            VideoClone = (Bitmap)Video.Clone();
+            original.Image = Video;
+            
+            if (GrayIsOn)
+            {
+                grayBmp = grayFilter.Apply(VideoClone);
+                process.Image = grayBmp;  
+            }
+
+            if (EdgeBmpIsOn)
+            {
+                grayBmp = grayFilter.Apply(VideoClone);
+                EdgeBmp = Sobeledgededector.Apply(grayBmp);
+                process.Image = EdgeBmp;
+            }
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog(); //shows the file dialog
+            Filename = openFileDialog1.FileName; //get the filename
+            OriginalBmp = new Bitmap(Filename);
+            original.Image = OriginalBmp;
+
+            this.Text = Filename;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            grayBmp = grayFilter.Apply(OriginalBmp);
+            process.Image = grayBmp;//set the picturebox
+            GrayIsOn = true;
+            //grayBmp = grayFilter.Apply(Video);
+            //process.Image = grayBmp;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //EdgeBmp = Sobeledgededector.Apply(grayBmp); //apply the edge dedector to the gray image
+            //process.Image = EdgeBmp; //set the pictureboc image to the edge bitmap
+            EdgeBmpIsOn = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int.TryParse(textBox1.Text,out Threshold);
+            BinFilter.ThresholdValue = Threshold;
+            BinBmp = BinFilter.Apply(grayBmp);
+            process.Image = BinBmp;
+            this.Text = Threshold.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            BlobFilter.MaxHeight = Convert.ToInt16( textBox2.Text.ToString());
+            BlobFilter.MinHeight = Convert.ToInt16(textBox3.Text.ToString());
+            BlobFilter.MaxWidth = Convert.ToInt16( textBox4.Text.ToString());
+            BlobFilter.MinWidth = Convert.ToInt16(textBox5.Text.ToString());
+            
+            BlobBmp = BlobFilter.Apply(BinBmp);
+            BlobCountFilter.ProcessImage(BlobBmp);
+            BlobArray = BlobCountFilter.GetObjects(BlobBmp, true); //apply blob counting 
+
+            process.Image = BlobBmp;
+            this.Text += ";" + BlobBmp.Width.ToString();
+            //this.Text = BlobArray[0].Area.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            videoSource.DesiredFrameSize = new Size(320, 40);
+            videoSource.Start();
+            videoSource.NewFrame += new NewFrameEventHandler(videoSource_NewFrame);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            circleTransform.ProcessImage(grayBmp);
+            Bitmap houghCircleTransform = circleTransform.ToBitmap();
+            process.Image = houghCircleTransform;
+        } 
+    }
+}
